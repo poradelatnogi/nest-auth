fba21fcf439da7d3c2f7a909bce2e27b9171f24c
Ihor Hrechan <poradelatnogi@gmail.com>
2023-03-17 10:46:47 +0200

    Added module

---
+++ src/nest-auth.module.ts
@@ -0,0 +1,20 @@
+import { DynamicModule, Module } from "@nestjs/common";
+import { NestAuthService } from "./nest-auth.service";
+import { NestAuthTypeOrmService } from "./nest-auth-type-orm.service";
+
+@Module({})
+export class NestAuthModule {
+  static register(): DynamicModule {
+    return {
+      module: NestAuthModule,
+      providers: [
+        NestAuthService,
+        NestAuthTypeOrmService,
+      ],
+      exports: [
+        NestAuthService,
+        NestAuthTypeOrmService,
+      ]
+    }
+  }
+}
